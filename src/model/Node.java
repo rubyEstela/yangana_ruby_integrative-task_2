c97a0a5fdb01d3a8fc4387cbe0f4fc125d041246
rubyEstela <ymedina.ruby@gmail.com>
2020-11-02 20:26:54 -0500

    creation of the matrix

---
+++ src/model/Node.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Node {
+	
+	private int row;
+	private int column;
+	private Node next;
+	private Node prev;
+	private Node up;
+	private Node down;
+	
+	public Node(int row, int column) {
+		
+		this.row = row;
+		this.column = column;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public char getNameolumn() {
+		return (char)('A'+column);
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node getPrev() {
+		return prev;
+	}
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+	public Node getUp() {
+		return up;
+	}
+	public void setUp(Node up) {
+		this.up = up;
+	}
+	public Node getDown() {
+		return down;
+	}
+	public void setDown(Node down) {
+		this.down = down;
+	}
+	
+	public String toString() {
+		return "[("+row+","+column+")]";
+	}
+
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+
+import model.Matrix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Matrix lm = new Matrix(2, 26);
+		System.out.println(lm);
+
+	}
+}
